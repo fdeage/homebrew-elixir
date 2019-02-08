4a51a0332b23257f041ec73022f0b4ccfa442846
Félix Déage <fdeage@gmail.com>
2019-02-08 11:36:13 +0100

    Remove bottles

--- Formula/elixir@1.7.3.rb
+++ Formula/elixir@1.7.3.rb
@@ -29,13 +29,6 @@ class ElixirAT173 < Formula
   sha256 "c9beabd05e820ee83a56610cf2af3f34acf3b445c8fabdbe98894c886d2aa28e"
   head "https://github.com/elixir-lang/elixir.git"
 
-  bottle do
-    sha256 "2d9d5faee079949f780c8f6a1ccba015d64ecf859ed87384ae4239d69be60142" => :high_sierra
-    sha256 "83e4a920d648d541288dd73b6cc19cb4d91a54e703609320e806c2efd6000466" => :sierra
-    sha256 "88f24d7781e7191d94b39fc36464781385a314656bb6900f42cbd996654b579d" => :el_capitan
-    sha256 "0d59995085bcdbdbfc08a6467ca99934598756b663c603cffc9103a96b36ff44" => :yosemite
-  end
-
   depends_on Erlang19Requirement
 
   def install
